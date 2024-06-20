df223330ef4e083e6e4ab5dc6e5570cee3294b93
shashank <ss9621963652@gmail.com>
2024-06-20 13:35:45 +0530

    vite-config-changes001

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
-// import { VitePWA } from "vite-plugin-pwa";
-// import path from "path";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Ensure Firebase aliases are correctly set
+      "firebase/app": "firebase/app",
+      "firebase/auth": "firebase/auth",
+      "firebase/firestore": "firebase/firestore",
+    },
+  },
 });
