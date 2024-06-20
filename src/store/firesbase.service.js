882825bd24b87cd368ba1739984690892d850a3e
shashank <ss9621963652@gmail.com>
2024-06-20 14:00:30 +0530

    vite-config-changes002

--- src/store/firesbase.service.js
+++ src/store/firesbase.service.js
@@ -1,29 +1,6 @@
-// import firebase from "firebase/compat/app";
-// import "firebase/compat/auth";
-// import "firebase/compat/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAYndPhOumY4Ai5PVGQLckYxvPAm1zRhSo",
-//   authDomain: "extramile-5c05c.firebaseapp.com",
-//   projectId: "extramile-5c05c",
-//   storageBucket: "extramile-5c05c.appspot.com",
-//   messagingSenderId: "257766069413",
-//   appId: "1:257766069413:web:6aa285b5234c655ae3294c",
-//   measurementId: "G-LTYTH23ZZX",
-// };
-
-// // Initialize Firebase
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const auth = firebase.auth();
-// const provider = new firebase.auth.GoogleAuthProvider();
-
-// export { auth, provider };
-// export default db;
-
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYndPhOumY4Ai5PVGQLckYxvPAm1zRhSo",
